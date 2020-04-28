0bfd754b88fce0645a8fd5188a0e1144289d5cf4
Lisov Pavel <lisovweb@gmail.com>
2020-04-28 22:49:34 +0300

    add container component

--- src/client/components/page/page.tsx
+++ src/client/components/page/page.tsx
@@ -1,13 +1,14 @@
 import React, { ReactNode } from "react";
+import { Container } from "uikit";
 
 import { PageContent } from "./page-content";
-import { Routes } from "client/config/routes";
+import { Routes } from "../../config/routes";
 
 export const Page = ({ children }: { children?: ReactNode }) => {
   return (
-    <div>
+    <Container>
       <PageContent routes={Routes} />
       {children}
-    </div>
+    </Container>
   );
 };
